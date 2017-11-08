4c40895666fc2ed96ed3bb1d3881520e29dd8326
chenchianstudent <105021007@live.asia.edu.tw>
2017-11-08 11:27:11 +0800

    55

---
+++ ex20171108.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 2017/11/8
+ * Time: 上午 10:04
+ */
+$var = 陳麒安105021007poi;
+$n=$_POST['n'];
+$m=$_POST['m'];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php echo $var ?>
+<table align="center" border="2">
+    <?php
+
+    for($i=1;$i<=$n;$i++){
+        echo"<tr>";
+        for($j=1;$j<=$m;$j++){
+            echo "<td style='width=30px' align='center'><span style='font-size: 1.2em'>". $i*$j. "</span></td>";
+        }
+        echo"</tr>";
+    }
+    ?>
+</table>
+
+</body>
+</html>
