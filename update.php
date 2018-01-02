5a75495d6f5a3cd546deb5e23275ae64463c408a
chenchianstudent <105021007@live.asia.edu.tw>
2018-01-02 22:05:56 +0800

    07

--- update.php
+++ update.php
@@ -18,9 +18,11 @@ if($_SESSION['name'] != null)
     $id = $_SESSION['name'];
     //若以下$id直接用$_SESSION['username']將無法使用
     $sql = "SELECT * FROM nani where name='$id'";
+    echo $sql."<br>";
     $result = mysqli_query($sql);
+    echo $result."<br>";
     $row = mysqli_fetch_row($result);
-
+    echo $row."<br>";
     echo "<form name=\"form\" method=\"post\" action=\"update_finish.php\">";
     echo "帳號：<input type=\"text\" name=\"id\" value=\"$row[1]\" />(此項目無法修改) <br>";
     echo "密碼：<input type=\"password\" name=\"pw\" value=\"$row[2]\" /> <br>";
