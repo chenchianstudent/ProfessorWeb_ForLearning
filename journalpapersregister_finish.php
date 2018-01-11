17e2c3e514049fa9e688d97e3ea31e2db653b840
chenchianstudent <105021007@live.asia.edu.tw>
2018-01-11 16:51:18 +0800

    55

---
+++ journalpapersregister_finish.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 2017/11/22
+ * Time: 上午 10:43
+ */
+?>
+<?php session_start(); ?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+include("mysql_connect.inc.php");
+$id = $_POST['id'];
+$pw = $_POST['pw'];
+$telephone = $_POST['telephone'];
+$address = $_POST['address'];
+$other=$_POST['other'];
+$other1=$_POST['other1'];
+$other2=$_POST['other2'];
+//判斷帳號密碼是否為空值
+//確認密碼輸入的正確性
+if($_SESSION['name'] != null)
+{
+    //新增資料進資料庫語法
+    $sql = "insert into journalpapers (jname, publish, eisci, name, time, ping) values ('$pw','$telephone', '$address','$other','$other1','$other2')";
+    if(mysqli_query($link,$sql))
+    {
+        echo '新增成功!';
+        echo '<meta http-equiv=REFRESH CONTENT=2;url=control4.php>';
+    }
+    else
+    {
+        echo '新增失敗!';
+        echo '<meta http-equiv=REFRESH CONTENT=2;url=control4.php>';
+    }
+}
+else
+{
+    echo '您無權限觀看此頁面!';
+    echo '<meta http-equiv=REFRESH CONTENT=2;url=pweb2.php>';
+}
+?>
